4fd9028eb1c4f990c96170ab5cac58c850f9a201
RagsToHrishes <hrishi.leen@gmail.com>
2025-10-07 08:50:05 -0400

    fix styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site: 'https://ragstohrishes.github.io',
-  base: '/ragstohrishes.github.io/'
+  base: '/'
 });
